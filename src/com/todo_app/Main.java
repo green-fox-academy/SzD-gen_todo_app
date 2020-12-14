520fc3fa5d4501ab05e1162747731d1bb61c3ff9
Your Name <szucsi.daniel123@gmail.com>
2020-12-14 18:48:13 +0100

    Kilistazas

--- src/com/todo_app/Main.java
+++ src/com/todo_app/Main.java
@@ -1,7 +1,15 @@
 package com.todo_app;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
         if (args.length == 0) {
             System.out.println("Command line application");
             System.out.println("=============================");
@@ -11,6 +19,27 @@ public class Main {
             System.out.println("    -a   Add new task");
             System.out.println("    -r   Remove a task");
             System.out.println("    -c   Complete task");
+        }
+        else if (args[0].equals("-l")){
+            scan(Paths.get("C:/Users/User/Documents/Greenfox/Todo/SzD-gen_todo_app/tennivalok.txt"));
+        }
+
+    }
+    static void scan (Path path){
+        try {
+            Scanner fileScanner = new Scanner(path);
+            int n = 1;
+            while (fileScanner.hasNextLine()) {
+                String data = fileScanner.nextLine();
+                System.out.println(n + " " + data);
+                n++;
+            }
+        }
+        catch (FileNotFoundException e){
+            System.out.println("File not found");
+        }
+        catch (IOException e){
+            System.out.println("Bad input");
         }
     }
 }
